1707c6fcc3503ff98e0aa6ddfd19d58f03794e98
Wisllor PS <79232049+Kevallion@users.noreply.github.com>
2026-03-16 19:12:45 +0100

    change

--- scripts/entities/spatial_station.gd
+++ scripts/entities/spatial_station.gd
@@ -1,9 +1,3 @@
 class_name SpatialStation extends SpaceObject
 
 @onready var interaction_area: InteractionArea = %InteractionArea
-@export var carburantToGive := 50.0
-
-
-
-func _on_interaction_area_interaction(body: Player) -> void:
-	body.refillGas()
